3520e6d3f8e1c9e9275a387ebb0f286b4b232b4f
Sara Youssef <sarary2000@gmail.com>
2022-10-19 21:42:14 +0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record){
+    const result = record.find(re => re.result === 'W')
+
+   if (result){
+        return result.year
+    }else{
+        return undefined
+}
+}
